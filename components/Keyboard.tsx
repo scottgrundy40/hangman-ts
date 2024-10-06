e740f06a32600aa8bb47cc14172a8ffb03b63548
Scott Grundy <scottgrundy40@hotmail.co.uk>
2024-10-06 16:23:21 +0200

    Add keyboard component and css styles

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -1,5 +1,24 @@
 import { KEYS } from "../utils/Keys";
+import styles from "../Keyboard.module.css";
 
 const keys = KEYS;
 
-export function Keyboard() {}
+export function Keyboard() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        return (
+          <button className={styles.btn} key={key}>
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
