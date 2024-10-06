85805ac356a609a717e6518108ce9285f560f1e3
Scott Grundy <scottgrundy40@hotmail.co.uk>
2024-10-06 16:00:31 +0200

    add HangmanWord component

--- HangmanWord.tsx
+++ HangmanWord.tsx
@@ -1,1 +1,28 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const guessdLetters = [""];
+  const word = "test";
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessdLetters.includes(letter) ? "visible" : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
